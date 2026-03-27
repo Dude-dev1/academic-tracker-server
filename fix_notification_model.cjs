f5645393daeea802f644799784d133f8558ea64c
Bigfredo270 <baahfrederick08@gmail.com>
2026-03-27 01:17:21 +0000

    Remove deprecated notification model update script

--- fix_notification_model.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const file = 'models/Notification.js';
-let content = fs.readFileSync(file, 'utf8');
-
-content = content.replace(/enum: \["assignment", "grade", "class", "task"\],/, 'enum: ["assignment", "grade", "class", "task", "announcement", "calendar", "notification"],');
-
-fs.writeFileSync(file, content);
